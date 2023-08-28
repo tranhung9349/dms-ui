44001d207492a24d02612095db36727e7fa3cf62
HùngTrần <36561063+tranhung9349@users.noreply.github.com>
2023-08-28 12:32:24 +0700

    update css

--- dns/js/treeview.js
+++ dns/js/treeview.js
@@ -1,10 +1,16 @@
 jQuery(document).ready(function () {
     $("#treeview .parent").click(function (e) {
         e.stopPropagation();
-        $(this).find(">ul").toggle("slow");
+        $(this).find(">ul ").toggle("slow");
         if ($(this).hasClass("close"))
             $(this).removeClass("close");
         else
             $(this).addClass("close");
+    });
+})
+
+jQuery(document).ready(function() {
+    $('input[type="checkbox"]').click(function(e) {
+        e.stopPropagation();
     });
 })
