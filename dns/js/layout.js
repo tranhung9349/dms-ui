f8d89b5c38fecbf3e4c29552792cc19ae2c21bab
HùngTrần <36561063+tranhung9349@users.noreply.github.com>
2023-08-30 23:11:13 +0700

    commit new page

--- dns/js/layout.js
+++ dns/js/layout.js
@@ -24,3 +24,27 @@ document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function() {
   $("body").tooltip({ selector: '[data-bs-toggle=tooltip]' });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const internalRows = document.querySelectorAll("#internalDocList tr td");
+  var lastTouchTime = 0;
+
+  internalRows.forEach(row => {
+    row.addEventListener("touchstart", () => {
+      var currentTime = new Date().getTime();
+      var timeSinceLastTouch = currentTime - lastTouchTime;
+      lastTouchTime = currentTime;
+
+      if (timeSinceLastTouch < 300) {
+        // Double click logic here;
+        let internalDocViewModal = new bootstrap.Modal(document.getElementById("internalDocViewModal"), {});
+        internalDocViewModal.show();
+      }
+        
+    });
+  });
+
+
+});
